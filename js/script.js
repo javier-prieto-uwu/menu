196efe2f15872b8e3e9e139812726ed25324a43c
javier-prieto-uwu <2302097@utrivieramaya.edu.mx>
2024-10-03 08:31:40 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function
+    (){
+        const navItems = document.querySelectorAll('.nav-item');
+        navItems.forEach((item, index) =>{
+            setTimeout(() => {
+                item.classList.add('show');
+            }, 100 * index);
+        })
+    }
+);
+
